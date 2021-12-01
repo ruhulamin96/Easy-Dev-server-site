685402e40150da0102c9771778c8b530a080d8b2
Ruhul Amin <ruhuliuice@gmail.com>
2021-12-01 18:37:28 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const { MongoClient } = require('mongodb');
+require('dotenv').config()
+const app = express()
+const port =process.env.PORT || 5000
+const cors=require('cors')
+
+//middleware
+app.use(cors())
+app.use(express.json())
+
+//
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.4ovqx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+
+async function run() {
+    try {
+      await client.connect();
+    //   const database = client.db("insertDB");
+    //   const haiku = database.collection("haiku");
+    console.log('connect successfull');
+     
+    } finally {
+    //   await client.close();
+    }
+  }
+  run().catch(console.dir);
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Server listening at Port ${port}`)
+})
